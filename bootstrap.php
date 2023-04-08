594db548e7b73fbe299668bd4efbadbd77e4b0c9
ZubyApp <109441625+ZubyApp@users.noreply.github.com>
2023-04-08 18:14:59 +0100

    Update bootstrap.php

--- bootstrap.php
+++ bootstrap.php
@@ -11,7 +11,7 @@ require __DIR__ . '/configs/path_constants.php';
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-$classLoader = new ClassLoader('DoctrineExtensions', __DIR__ . '/vendor/beberlei/doctrineextensions');
+$classLoader = new ClassLoader('doctrineextensions', __DIR__ . '/vendor/beberlei/doctrineextensions');
 $classLoader->register();
 
 return require CONFIG_PATH . '/container/container.php';
