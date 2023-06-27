3648dd9368d502624a8cb3702c050e2c6a203ef1
ZubyApp <109441625+ZubyApp@users.noreply.github.com>
2023-06-27 12:09:15 +0100

    Updates to modals

--- app/Contracts/UserInterface.php
+++ app/Contracts/UserInterface.php
@@ -4,8 +4,12 @@ declare(strict_types = 1);
 
 namespace App\Contracts;
 
+use App\Enum\UserRole;
+
 interface UserInterface
 {
     public function getId(): int;
     public function getPassword(): string;
+    public function getFirstname(): string;
+    public function getUserRole(): ?UserRole;
 }
